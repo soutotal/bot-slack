086ec173dd681fb3fbcb147a66fe6848c7f90bb9
Jonata Weber <jonataa@gmail.com>
2016-06-04 15:57:13 -0300

    Refatorar o código

--- index.js
+++ index.js
@@ -39,9 +39,8 @@ reply(
   'O próximo encontro será no dia 04/06 às 14:00 - estamos até barganhando um coffe break...'
 );
 
-controller.hears(
-  ["Evento"],
+reply(
+  ["evento"],
   ["direct_message","direct_mention","mention","ambient"],
-  function(bot,message) {
-    bot.reply(message,'Dia 31/05 as 18:00 teremos Aniversariantes do mês!! Vai ter :cake: :cake: :cake: :cake: :cake: :cake: :cake: \n Dia 03/06 as 13h teremos um bate-papo na sala do Oikos sobre a "Importância dos Testes de Software" \n Dia 04/06 as 14:00 acontecerá também o próximo encontro dos #loucosporconhecimento \n ');
-  });
+  'Dia 31/05 as 18:00 teremos Aniversariantes do mês!! Vai ter :cake: :cake: :cake: :cake: :cake: :cake: :cake: \n Dia 03/06 as 13h teremos um bate-papo na sala do Oikos sobre a "Importância dos Testes de Software" \n Dia 04/06 as 14:00 acontecerá também o próximo encontro dos #loucosporconhecimento \n'
+);
