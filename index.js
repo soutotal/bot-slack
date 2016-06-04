14102be5888acfef3332dd33e0ce2ae15659bb41
Jonata Weber <jonataa@gmail.com>
2016-06-04 15:49:37 -0300

    Refatorar o código

--- index.js
+++ index.js
@@ -17,14 +17,23 @@ controller.setupWebserver(process.env.PORT || 3001, function(err, webserver) {
     });
 });
 
-controller.hears(["ultimo encontro","último encontro"],["direct_message","direct_mention","mention","ambient"],function(bot,message) {
-  bot.reply(message,'SHOW DE BOLA. Quer saber? Venha conferir :P \n No último encontro fizemos um brainstorm das próximas atividades que serão: HACKEAR A TOTAL! :smiling_imp: ');
-});
+controller.hears(
+  ["ultimo encontro","último encontro"],
+  ["direct_message","direct_mention","mention","ambient"],
+  function(bot,message) {
+    bot.reply(message,'SHOW DE BOLA. Quer saber? Venha conferir :P \n No último encontro fizemos um brainstorm das próximas atividades que serão: HACKEAR A TOTAL! :smiling_imp:');
+  });
 
-controller.hears(["proximo encontro","próximo encontro"],["direct_message","direct_mention","mention","ambient"],function(bot,message) {
-  bot.reply(message,'O próximo encontro será no dia 04/06 às 14:00 - estamos até barganhando um coffe break...');
-});
+controller.hears(
+    ["proximo encontro","próximo encontro"],
+    ["direct_message","direct_mention","mention","ambient"],
+    function(bot,message) {
+      bot.reply(message,'O próximo encontro será no dia 04/06 às 14:00 - estamos até barganhando um coffe break...');
+  });
 
-controller.hears(["Evento"],["direct_message","direct_mention","mention","ambient"],function(bot,message) {
-  bot.reply(message,'Dia 31/05 as 18:00 teremos Aniversariantes do mês!! Vai ter :cake: :cake: :cake: :cake: :cake: :cake: :cake: \n Dia 03/06 as 13h teremos um bate-papo na sala do Oikos sobre a "Importância dos Testes de Software" \n Dia 04/06 as 14:00 acontecerá também o próximo encontro dos #loucosporconhecimento \n ');
-});
+controller.hears(
+  ["Evento"],
+  ["direct_message","direct_mention","mention","ambient"],
+  function(bot,message) {
+    bot.reply(message,'Dia 31/05 as 18:00 teremos Aniversariantes do mês!! Vai ter :cake: :cake: :cake: :cake: :cake: :cake: :cake: \n Dia 03/06 as 13h teremos um bate-papo na sala do Oikos sobre a "Importância dos Testes de Software" \n Dia 04/06 as 14:00 acontecerá também o próximo encontro dos #loucosporconhecimento \n ');
+  });
