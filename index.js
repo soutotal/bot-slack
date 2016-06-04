01f90e23f2470f1749f13df626e76169b6587f02
Jonata Weber <jonataa@gmail.com>
2016-06-04 15:58:08 -0300

    Refatorar o código

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ controller.setupWebserver(process.env.PORT || 3001, function(err, webserver) {
     });
 });
 
-function reply (patterns, eventos, text) {
+function reply (controller, patterns, eventos, text) {
   controller.hears(
     patterns,
     eventos,
@@ -28,18 +28,21 @@ function reply (patterns, eventos, text) {
 }
 
 reply(
+  controller,
   ["ultimo encontro","último encontro"], 
   ["direct_message","direct_mention","mention","ambient"],
   'SHOW DE BOLA. Quer saber? Venha conferir :P \n No último encontro fizemos um brainstorm das próximas atividades que serão: HACKEAR A TOTAL! :smiling_imp:'
 );
 
 reply(
+  controller,
   ["proximo encontro","próximo encontro"],
   ["direct_message","direct_mention","mention","ambient"],
   'O próximo encontro será no dia 04/06 às 14:00 - estamos até barganhando um coffe break...'
 );
 
 reply(
+  controller,
   ["evento"],
   ["direct_message","direct_mention","mention","ambient"],
   'Dia 31/05 as 18:00 teremos Aniversariantes do mês!! Vai ter :cake: :cake: :cake: :cake: :cake: :cake: :cake: \n Dia 03/06 as 13h teremos um bate-papo na sala do Oikos sobre a "Importância dos Testes de Software" \n Dia 04/06 as 14:00 acontecerá também o próximo encontro dos #loucosporconhecimento \n'
